a19788553634a47178062873269f3a04d95ef93c
Grant Hooks <grant.hooks@ymail.com>
2017-10-02 19:22:06 -0700

    Bubble Sort

--- H3/BubbleSort.cpp
+++ H3/BubbleSort.cpp
@@ -1,80 +1,96 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-template <typename T>
-void printArray(T array[], int n, int left, int right) {
-  cout << "Hole Array: ";
+ofstream fout;
+
+void printWorkedArray(char array[], int n, int left, int right) {
+  cout << "Whole Array: ";
+  fout << "Whole Array: ";
   for(int i = 0; i < n; i++){
     cout << array[i] << " ";
+    fout << array[i] << " ";
   }
   cout << endl << "Worked Portion: ";
+  fout << endl << "Worked Portion: ";
   for(int i = left; i < right; i++){
     cout << array[i] << " ";
+    fout << array[i] << " ";
   }
   cout << endl;
+  fout << endl;
 }
 
-template <typename T>
-void bubbleSort(T array[], int n) {
+void printArray(char array[], int n) {
+  for(int i = 0; i < n; i++){
+    cout << array[i] << " ";
+    fout << array[i] << " ";
+  }
+  cout << endl;
+  fout << endl;
+}
+
+void bubbleSort(char array[], int n) {
   bool swapped = true;
   int j = 0;
   int k = n;
-  char tmp;
+  char temp;
   while (swapped) {
     swapped = false;
-    for (int i = j; i < k; i++) {
+    for (int i = j; i < k-1; i++) {
       if (array[i] > array[i + 1]) {
-        tmp = array[i];
+        temp = array[i];
         array[i] = array[i + 1];
-        array[i + 1] = tmp;
+        array[i + 1] = temp;
         swapped = true;
       }
     }
     cout << "Left to Right:" << endl;
-    printArray(array, n, j, k);
+    fout << "Left to Right:" << endl;
+    printWorkedArray(array, n, j, k);
 		k--;
 
-    if(swapped) {
-      swapped = false;
-      for (int i = k; i > j; i--) {
-        if (array[i] < array[i - 1]) {
-          tmp = array[i];
-          array[i] = array[i - 1];
-          array[i - 1] = tmp;
-          swapped = true;
-        }
+    for (int i = k; i > j; i--) {
+      if (array[i] < array[i - 1]) {
+        temp = array[i];
+        array[i] = array[i - 1];
+        array[i - 1] = temp;
+        swapped = true;
       }
-      cout << "Right to Left:" << endl;
-      printArray(array, n, j, k);
-		  j++;
     }
+    cout << "Right to Left:" << endl;
+    fout << "Right to Left:" << endl;
+    printWorkedArray(array, n, j, k);
+		j++;
   }
 }
 
-template <typename T>
-void readArray (T array[], int n) {
+void readArray (char array[], int n) {
   cout << "Enter Array: ";
   for (int i = 0; i < n; i++) {
     cin >> array[i];
   }
+  printWorkedArray(array, n, 0, 0);
   bubbleSort(array, n);
 }
 
 int main(int argc, char const *argv[]) {
   int n;
-  string type;
+
   cout << "Enter the length of the array: ";
   cin >> n;
-  cout << "Enter type of Elements(char, int): ";
-  cin >> type;
-  if(type == "char") {
-    char array[n];
-    readArray(array, n);
-  }else if(type == "int") {
-    int array[n];
-    readArray(array, n);
+
+  char array [n];
+  cout << "Enter Array: ";
+  for (int i = 0; i < n; i++) {
+    cin >> array[i];
   }
+  fout.open("log.txt");
+  printArray(array, n);
+  bubbleSort(array, n);
+  printArray(array, n);
+  fout.close();
 
   return 0;
 }
