762d8343143d84495df8657b491b12d5dde15fda
Grant Hooks <grant.hooks@ymail.com>
2017-11-07 14:08:36 -0800

    H5

---
+++ H5/h5.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int weeks = 4;
+string choices [4];
+string solution [4];
+int values [4];
+int totals [4];
+int high [4] = {5, 50, 5, 1};
+int low [4] = {10, 1, 10, 10};
+
+void OptimalPlanB(int weeks) {
+  for(int i = 0; i < weeks; i++) {
+    if(i > 0) {
+      if(high[i] > low[i] + low[i-1]) {
+        values[i] = high[i];
+        totals[i] = high[i] + totals[i-2];
+      }else {
+        values[i] = low[i];
+        totals[i] = low[i] + totals[i-1];
+      }
+    }else {
+      if(high[i] > low[i]) {
+        values[i] = high[i];
+        totals[i] = high[i] + totals[i-1];
+      }else {
+        values[i] = low[i];
+        totals[i] = low[i] + totals[i-1];
+      }
+    }
+  }
+}
+
+void OptimalPlanC(int weeks) {
+  for(int i = 0; i < weeks; i++) {
+    if(i > 0) {
+      if(high[i] > low[i] + low[i-1]) {
+        values[i] = high[i];
+        choices[i] = "High";
+      }else {
+        values[i] = low[i];
+        choices[i] = "Low";
+      }
+    }else {
+      if(high[i] > low[i]) {
+        values[i] = high[i];
+        choices[i] = "High";
+      }else {
+        values[i] = low[i];
+        choices[i] = "Low";
+      }
+    }
+  }
+}
+
+void OptimalPlanD(int weeks) {
+  for(int i = 0; i < weeks; i++) {
+    if(i > 0) {
+      if(high[i] > low[i] + low[i-1]) {
+        values[i] = high[i];
+        choices[i] = "High";
+        solution[i] = "High";
+        solution[i-1] = "Rest";
+      }else {
+        values[i] = low[i];
+        choices[i] = "Low";
+        solution[i] = "Low";
+      }
+    }else {
+      if(high[i] > low[i]) {
+        values[i] = high[i];
+        choices[i] = "High";
+        solution[i] = "High";
+      }else {
+        values[i] = low[i];
+        choices[i] = "Low";
+        solution[i] = "Low";
+      }
+    }
+  }
+}
+
+template <typename T>
+void PrintTable(T table[4]) {
+  for(int i = 0; i < weeks; i++) {
+    cout << table[i] << " ";
+  }
+  cout << endl;
+}
+
+int main() {
+  OptimalPlanB(weeks);
+  PrintTable(values);
+  PrintTable(totals);
+  OptimalPlanC(weeks);
+  PrintTable(choices);
+  OptimalPlanD(weeks);
+  PrintTable(solution);
+}
